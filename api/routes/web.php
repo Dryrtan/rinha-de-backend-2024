bc6956e2d774daf7be3c6cfaaac346262f1bb59b
Dryrtan <kleidimars@gmail.com>
2024-02-25 23:29:18 +0100

    Route /clientes/{id_client}/transacoes

--- api/routes/web.php
+++ api/routes/web.php
@@ -20,7 +20,53 @@ $router->get('/', function () use ($router) {
 
 $router->get('/clientes/{id_client}/extrato', function ($id_client) {
     $results = app('db')->select('SELECT limite FROM clientes WHERE id = ?', [$id_client]);
+
     return response()->json([
         'limite' => $results[0]->limite
     ]);
+});
+
+$router->post('/clientes/{id_client}/transacoes', function ($id_client) {
+    $transaction_value = request('valor');
+    $transaction_type = request('tipo');
+    $transaction_descricao = request('descricao');
+
+    $islocked = app('db')->select('SELECT acquire_lock(?)', [$id_client]);
+
+    if ($islocked[0]->acquire_lock) {
+        $client_details = app('db')->select(
+            'SELECT c.id, c.limite, s.saldo FROM clientes c join saldos s ON c.id = s.cliente_id WHERE c.id = ?',
+            [$id_client]
+        );
+        $client_limit = $client_details[0]->limite;
+        $client_ammout = $client_details[0]->saldo;
+
+        if ($transaction_type == 'c') {
+            app('db')->update('UPDATE saldos SET saldo = saldo + ? WHERE cliente_id = ?', [$transaction_value, $id_client]);
+            app('db')->update("INSERT INTO transacoes (cliente_id, tipo, valor, descricao) VALUES ($id_client,'$transaction_type',$transaction_value,'$transaction_descricao')");
+        } else {
+            $ammout_available = $client_limit - $transaction_value;
+            if ($ammout_available < ($client_limit - $client_limit)) {
+                return response()->json([
+                    'mensage' => 'Este cliente não possui limite para este valor.'
+                ], 422);
+            }
+            app('db')->update(
+                'UPDATE saldos SET saldo = ? WHERE cliente_id = ?',
+                [$client_ammout - $transaction_value, $id_client]
+            );
+        }
+
+        app('db')->select('SELECT release_lock(?)', [$id_client]);
+
+        return response()->json([
+            'limite' => 0,
+            'saldo' => 0
+        ], 200);
+    }
+
+    return response()->json([
+        'status' => 'error',
+        'message' => $islocked[0]
+    ], 512);
 });
--- script.sql
+++ script.sql
@@ -1,4 +1,10 @@
--- Criar tabela clientes no postgres
+drop table if exists clientes cascade;
+drop table if exists saldos;
+drop table if exists transacoes;
+drop table if exists locks;
+drop function if exists acquire_lock;
+drop function if exists release_lock;
+
 CREATE TABLE clientes (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
@@ -7,9 +13,21 @@ CREATE TABLE clientes (
 
 CREATE TABLE saldos (
     id SERIAL PRIMARY KEY,
-    cliente_id INTEGER NOT NULL REFERENCES clientes(id),
-    saldo INTEGER NOT NULL
-    CONSTRAINT fk_clientes FOREIGN KEY(cliente_id) REFERENCES clientes(id)
+    cliente_id INTEGER NOT NULL,
+    saldo INTEGER NOT NULL,
+    CONSTRAINT fk_clientes_saldos_id
+		FOREIGN KEY (cliente_id) REFERENCES clientes(id)
+);
+
+CREATE TABLE transacoes (
+    id SERIAL PRIMARY KEY,
+    cliente_id INTEGER NOT NULL,
+    tipo VARCHAR(1) NOT NULL,
+    valor INTEGER NOT NULL,
+    descricao VARCHAR(10) NOT NULL,
+    realizada_em TIMESTAMP NOT NULL DEFAULT NOW(),
+    CONSTRAINT fk_clientes_transacoes_id
+		FOREIGN KEY (cliente_id) REFERENCES clientes(id)
 );
 
 CREATE TABLE locks (
@@ -40,7 +58,6 @@ BEGIN
 END
 $$ LANGUAGE plpgsql;
 
--- População de dados
 INSERT INTO clientes (nome, limite)
 VALUES
 ('Berta Ralha', 1000 * 100),
@@ -48,3 +65,11 @@ VALUES
 ('Felisbino Pamonha', 10000 * 100),
 ('Dolores Encrenca', 100000 * 100),
 ('Hortênsia Risonha', 5000 * 100);
+
+INSERT INTO saldos (cliente_id, saldo)
+VALUES
+(1, 0),
+(2, 0),
+(3, 0),
+(4, 0),
+(5, 0);
